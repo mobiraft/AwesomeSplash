fc3b8243394e53f8aa7f0fabfe3aaf2c4f58343c
hardik parmar <hardik16432@gmail.com>
2020-08-31 19:02:25 +0530

    Initial Commit

---
+++ AwesomeSplash/AwesomeSplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeSplashApp.swift
+//  AwesomeSplash
+//
+//  Created by Hardik Parmar on 31/08/20.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeSplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
