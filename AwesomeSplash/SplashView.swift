d88aefeff2f22973ad34e57b4dc7e580fc8dad06
hardik parmar <hardik16432@gmail.com>
2020-08-31 20:43:56 +0530

    First commit

---
+++ AwesomeSplash/SplashView.swift
@@ -0,0 +1,45 @@
+//
+//  SplashView.swift
+//  AwesomeSplash
+//
+//  Created by Hardik Parmar on 31/08/20.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    
+    // 1.
+    @State var isActive:Bool = false
+    
+    var body: some View {
+        VStack {
+            // 2.
+            if self.isActive {
+                // 3.
+                HomeView()
+            } else {
+                // 4.
+                Text("Awesome Splash Screen!")
+                    .font(Font.largeTitle)
+            }
+        }
+        // 5.
+        .onAppear {
+            // 6.
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                // 7.
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+    
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
